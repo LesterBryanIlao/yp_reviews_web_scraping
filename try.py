70b1444d7b65b377d19e8eafafb922eef2de9718
LesterBryanIlao <lestertetengilao@gmail.com>
2023-08-11 21:49:28 +0800

    feat: get all details title in dl element of a page

--- try.py
+++ try.py
@@ -2,34 +2,28 @@ from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
-from utils.scraping_utils import ScrapeUtil
-from main import get_driver, convert_csv_file_to_list
+from utils.scraping_utils import ScrapeUtil, get_more_details, convert_csv_file_to_list
+
 # collected_links = ScrapeUtil.get_links(csv_files=['NV_hotel_links.csv', 'NY_hotel_links.csv'], output_file='test.csv')
 #
 # collected_links = ScrapeUtil.remove_duplicates(collected_links)
 
 
-driver = get_driver()
+driver = ScrapeUtil.get_driver()
 temp_list = convert_csv_file_to_list('NV_hotel_links.csv')
 # print(temp_list[:10])
 
-def get_detail(mode: By, xpath: str):
-	detail = None
-	try:
-		detail = driver.find_element(mode, xpath).text
-	except NoSuchElementException or AttributeError:
-		return None
-	return detail
+
 
 """data to collect: name, contact_number, address, yp_rating, reviews, payment_method, language"""
 driver.get(temp_list[1])
 
-name = get_detail(By.XPATH, '//*[@id="main-header"]/article/div/h1')
-contact_number = get_detail(By.XPATH, '//*[@id="default-ctas"]/a[1]/strong')
-address = get_detail(By.XPATH, '//*[@id="default-ctas"]/a[3]/span')
-yp_rating_count = get_detail(By.XPATH, '//*[@id="main-header"]/article/section/div[2]/section/a[1]/span')
-reviews_count = get_detail(By.XPATH, '//*[@id="main-header"]/article/section/div[2]/section/a[2]/span')
-dl_location = '//*[@id="business-info"]/dl'
-services_products_xpath = '//*[@id="business-info"]/dl/dt[4]'
-services_products = get_detail(By.XPATH, '//*[@id="business-info"]/dl/dd[4]/text()')
-print([name, contact_number, address, yp_rating_count, reviews_count])
+# name = ScrapeUtil.get_detail(By.XPATH, '//*[@id="main-header"]/article/div/h1', driver)
+# contact_number = ScrapeUtil.get_detail(By.XPATH, '//*[@id="default-ctas"]/a[1]/strong', driver)
+# address = ScrapeUtil.get_detail(By.XPATH, '//*[@id="default-ctas"]/a[3]/span', driver)
+# yp_rating_count = ScrapeUtil.get_detail(By.XPATH, '//*[@id="main-header"]/article/section/div[2]/section/a[1]/span', driver)
+# reviews_count = ScrapeUtil.get_detail(By.XPATH, '//*[@id="main-header"]/article/section/div[2]/section/a[2]/span', driver)
+dl_location = driver.find_element(By.XPATH, '//*[@id="business-info"]/dl')
+dt_contents = get_more_details(dl_location, By.TAG_NAME, 'dt')
+print(dt_contents)
+# print([name, contact_number, address, yp_rating_count, reviews_count])
--- utils/scraping_utils.py
+++ utils/scraping_utils.py
@@ -1,45 +1,169 @@
 import csv
+import time
+from configparser import ConfigParser
+
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 
 class ScrapeUtil:
+	@staticmethod
+	def get_links(csv_files: list, output_file: str) -> list:
+		temp_list = []
+		for file in csv_files:
+			ScrapeUtil._validate_csv_file(file)
+			links = ScrapeUtil._extract_links(file)
+			temp_list.extend(links)
 
-    @staticmethod
-    def get_links(csv_files: list, output_file: str) -> list:
-        temp_list = []
-        for file in csv_files:
-            ScrapeUtil._validate_csv_file(file)
-            links = ScrapeUtil._extract_links(file)
-            temp_list.extend(links)
+		ScrapeUtil._write_links_to_csv(temp_list, output_file)
 
-        ScrapeUtil._write_links_to_csv(temp_list, output_file)
+		return temp_list
 
-        return temp_list
+	@staticmethod
+	def _validate_csv_file(file: str) -> None:
+		if not file.endswith('.csv'):
+			raise ValueError("All files must be of type .csv")
 
-    @staticmethod
-    def _validate_csv_file(file: str) -> None:
-        if not file.endswith('.csv'):
-            raise ValueError("All files must be of type .csv")
+	@staticmethod
+	def _extract_links(file: str) -> list:
+		links = []
+		with open(file, 'r', newline='') as f:
+			reader = csv.reader(f, delimiter=',')
+			for row in reader:
+				ScrapeUtil._validate_link_format(row[0])
+				links.append(row[0])
+		return links
 
-    @staticmethod
-    def _extract_links(file: str) -> list:
-        links = []
-        with open(file, 'r', newline='') as f:
-            reader = csv.reader(f, delimiter=',')
-            for row in reader:
-                ScrapeUtil._validate_link_format(row[0])
-                links.append(row[0])
-        return links
+	@staticmethod
+	def _validate_link_format(link: str) -> None:
+		if not link.startswith('http'):
+			raise ValueError("All links must start with 'http'")
 
-    @staticmethod
-    def _validate_link_format(link: str) -> None:
-        if not link.startswith('http'):
-            raise ValueError("All links must start with 'http'")
+	@staticmethod
+	def _write_links_to_csv(links: list, output_file: str) -> None:
+		with open(output_file, 'w', newline='') as csvfile:
+			writer = csv.writer(csvfile, delimiter=',')
+			for link in links:
+				writer.writerow([link])
 
-    @staticmethod
-    def _write_links_to_csv(links: list, output_file: str) -> None:
-        with open(output_file, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            for link in links:
-                writer.writerow([link])
-    @staticmethod
-    def remove_duplicates(links: list) -> list:
-        return list(set(links))
+	@staticmethod
+	def remove_duplicates(links: list) -> list:
+		return list(set(links))
+
+	@staticmethod
+	def get_detail(mode: By, xpath: str, driver: webdriver) -> str or None:
+		detail = None
+		try:
+			detail = driver.find_element(mode, xpath).text
+		except NoSuchElementException or AttributeError:
+			return None
+		return detail
+
+	@staticmethod
+	def get_driver() -> webdriver:
+		parser = ConfigParser()
+		parser.read('project.config')
+
+		web_driver_path = parser.get('webdriver', 'driver_file_path')
+
+		service = Service(web_driver_path)
+
+		chrome_options = Options()
+		chrome_options.add_argument("--headless")
+
+		driver = webdriver.Chrome(service=service, options=chrome_options)
+		# driver = webdriver.Chrome(service=service)
+
+		return driver
+WAIT_TIME = 20
+
+
+def collect_links(url) -> list:
+	"""Collects all the links from the given url."""
+	driver = ScrapeUtil.get_driver()
+
+	driver.get(url)
+	driver.implicitly_wait(WAIT_TIME)
+	driver.maximize_window()
+
+	start_time = time.time()
+
+	hotels = []
+
+	next_page_button = driver.find_element(By.XPATH, "//a[@class='next ajax-page']")
+
+	while len(hotels) < 500 and next_page_button.is_displayed():
+		wait = WebDriverWait(driver, WAIT_TIME)
+		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+		temp_hotel_links = driver.find_elements(By.XPATH, "//a[@class='business-name']")
+		for link in temp_hotel_links:
+			try:
+				true_link = link.get_attribute('href')
+				print(f"Adding {true_link}")
+				hotels.append(true_link)
+			except Exception as e:
+				print(e)
+				continue
+		try:
+			driver.refresh()
+			next_page_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@class='next ajax-page']")))
+			next_page_button.click()
+		except Exception as e:
+			print(e)
+			break
+	print("Execution time: {} seconds".format(time.time() - start_time))
+
+	return hotels
+
+
+def write_to_csv_file(data: list[str], output_file: str):
+	"""Writes the given data to the given output file.
+
+	Args:
+    data: The data to write to the file.
+    output_file: The path to the output file.
+    """
+	for _ in data:
+		with open(output_file, 'a', newline='') as file:
+			writer = csv.writer(file)
+			writer.writerow([_])
+def convert_csv_file_to_list(file_name: str) -> list[str]:
+	"""Converts the given csv file to a list.
+
+	Args:
+	file_name: The path to the csv file.
+
+	Returns:
+	A list containing the data from the csv file.
+	"""
+	data = []
+	with open(file_name, newline='') as file:
+		reader = csv.reader(file)
+		for row in reader:
+			data.append(row[0])
+	return data
+
+def get_more_details(parent, mode: By, attrribute: str):
+    """Gets the details from the given parent element.
+
+    Args:
+        parent: The parent element to search from.
+        mode: The mode to search by.
+
+    Returns:
+        The details from the given parent element.
+    """
+    try:
+        details = parent.find_elements(mode, attrribute)
+
+        print("Found details")
+        return [] if len(details) == 0 else [_.text for _ in details]
+    except (NoSuchElementException, AttributeError):
+	    print("No details found")
+	    return []
